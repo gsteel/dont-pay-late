b63cbe67e6aa076ade7040d1784d4cbf824fdab8
George Steel <george@net-glue.co.uk>
2022-09-14 22:11:19 +0100

    Update application entry point to work with Mezzio

--- public/index.php
+++ public/index.php
@@ -1,10 +1,27 @@
 <?php
+
+declare(strict_types=1);
+
+// Delegate static file requests back to the PHP built-in webserver
+use Mezzio\Application;
+use Mezzio\MiddlewareFactory;
+use Psr\Container\ContainerInterface;
+
+if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
+    return false;
+}
+
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-/** @var \Interop\Container\ContainerInterface $container */
-$container = require 'config/container.php';
+(static function (): void {
+    $container = require __DIR__ . '/../config/container.php';
+    assert($container instanceof ContainerInterface);
 
-/** @var \Zend\Expressive\Application $app */
-$app = $container->get(\Zend\Expressive\Application::class);
-$app->run();
+    $app = $container->get(Application::class);
+    $factory = $container->get(MiddlewareFactory::class);
+    (require __DIR__ . '/../config/pipeline.php')($app, $factory, $container);
+    (require __DIR__ . '/../config/routes.php')($app, $factory, $container);
+
+    $app->run();
+})();
